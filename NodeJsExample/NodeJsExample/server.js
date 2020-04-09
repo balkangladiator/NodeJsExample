c2eaef94da4c77f915bcdb673c0ec07ceb8af701
Wielaard <jilles.wielaard@kpn.com>
2020-04-09 16:11:52 +0200

    added message

--- NodeJsExample/NodeJsExample/server.js
+++ NodeJsExample/NodeJsExample/server.js
@@ -1,8 +1,11 @@
-'use strict';
-var http = require('http');
+"use strict";
+var http = require("http");
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//hoi cengiz
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port);
