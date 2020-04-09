e75fec969a4bf809c1916290e3c3619f9ff0d8cb
cengiz <balkan_gladiator@hotmail.com>
2020-04-09 16:17:01 +0200

    Changed comment

--- NodeJsExample/NodeJsExample/server.js
+++ NodeJsExample/NodeJsExample/server.js
@@ -2,7 +2,7 @@
 var http = require("http");
 var port = process.env.PORT || 1337;
 
-//hoi cengiz
+//jow jillie
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
